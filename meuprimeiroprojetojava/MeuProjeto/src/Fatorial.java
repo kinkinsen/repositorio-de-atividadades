9854b0d36797f1bb9fca2e08b52981bf8ee04270
kinkinsen <allenkinsens@gmail.com>
2023-04-28 18:35:48 -0300

    init inicial

---
+++ meuprimeiroprojetojava/MeuProjeto/src/Fatorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Fatorial {
+   public static void main (String[] args) {
+    int multiplicacao = 1;
+    int fatorial;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Fatorial: ");
+    fatorial = scan.nextInt();
+    for(int i = fatorial;i>=1;i--){
+        multiplicacao= multiplicacao*i;
+    }
+    System.out.println(fatorial+"!="+multiplicacao);
+   }
+}
